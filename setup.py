71bde4e5733aebd255e2a544b66d8b03f6c14f2f
Chris Lamb <chris@chris-lamb.co.uk>
2016-03-21 01:01:09 +0800

    Releasing version 2.0.4

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name='django-switch-templatetag',
 
     url="https://chris-lamb.co.uk/projects/django-switch-templatetag",
-    version='2.0.3',
+    version='2.0.4',
     description="Simple switch tag for Django templates.",
 
     author="Chris Lamb",
